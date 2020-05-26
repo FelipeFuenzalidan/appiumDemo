27bbaf6c493b1fe81b47dac43e370160788bdb4f
Felipe Fuenzalida <ffuenzal@dtvpan.com>
2020-05-26 16:39:10 -0400

    first commit

--- src/test/java/com/directv/selfcareapp/pages/LoginPage.java
+++ src/test/java/com/directv/selfcareapp/pages/LoginPage.java
@@ -7,47 +7,47 @@ import org.openqa.selenium.WebElement;
 public class LoginPage extends BasePage {
 
     @AndroidFindBy(uiAutomator = "new UiSelector().text(\"E-mail\")")
-    @iOSFindBy(xpath = "")
+    @iOSFindBy(xpath = "demo")
     private WebElement usernameField;
 
     @AndroidFindBy(uiAutomator = "new UiSelector().text(\"Contraseña\")")
-    @iOSFindBy(xpath = "")
+    @iOSFindBy(xpath = "demo")
     private WebElement passwordField;
 
     @AndroidFindBy(uiAutomator = "new UiSelector().text(\"INGRESAR\")")
-    @iOSFindBy(xpath = "")
+    @iOSFindBy(xpath = "demo")
     private WebElement ingresarButton;
 
     @AndroidFindBy(xpath = "//*[contains(@text, 'incorrectas')]")
-    @iOSFindBy(xpath = "")
+    @iOSFindBy(xpath = "demo")
     private WebElement msgFailedLogin;
 
     @AndroidFindBy(xpath = "//*[contains(@text, 'PAGAR FACTURA')]")
-    @iOSFindBy(xpath = "")
+    @iOSFindBy(xpath = "demo")
     private WebElement pagarFacturaButton;
 
     @AndroidFindBy(xpath = "//*[contains(@text, 'Ayuda')]")
-    @iOSFindBy(xpath = "")
+    @iOSFindBy(xpath = "demo")
     private WebElement ayudaButton;
 
     @AndroidFindBy(xpath = "//*[contains(@text, 'ENVIAR MENSAJE')]")
-    @iOSFindBy(xpath = "")
+    @iOSFindBy(xpath = "demo")
     private WebElement enviarMensajeButton;
 
     @AndroidFindBy(id = "com.android.chrome:id/url_bar")
-    @iOSFindBy(xpath = "")
+    @iOSFindBy(xpath = "demo")
     private WebElement urlWhatsApp;
 
     @AndroidFindBy(uiAutomator = "new UiSelector().text(\"Mis datos\")")
-    @iOSFindBy(xpath = "")
+    @iOSFindBy(xpath = "demo")
     private WebElement misDatosMenu;
 
     @AndroidFindBy(uiAutomator = "new UiSelector().text(\"LOGOUT\")")
-    @iOSFindBy(xpath = "")
+    @iOSFindBy(xpath = "demo")
     private WebElement logoutButton;
 
     @AndroidFindBy(xpath = "//*[contains(@text, 'INICIAR SESIÓN')]")
-    @iOSFindBy(xpath = "")
+    @iOSFindBy(xpath = "demo")
     private WebElement iniciarSesionLabel;
 
 
