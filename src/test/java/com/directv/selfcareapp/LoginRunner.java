fedc87eed9e99831b5bbdb7ad91fe19900aa0e0b
Felipe Fuenzalida <ffuenzal@dtvpan.com>
2020-06-09 16:48:52 -0400

    first commit

--- src/test/java/com/directv/selfcareapp/LoginRunner.java
+++ src/test/java/com/directv/selfcareapp/LoginRunner.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.WebDriver;
 
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
-        plugin = {"pretty", "html:target/cucumber-html-report"},
+        plugin = {"pretty", "html:target/cucumber-html-report", "junit:target/cucumber-reports/Cucumber.xml"},
         features = {"src/test/resources/features/login.feature"}
 )
 public class LoginRunner {
